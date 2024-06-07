1da4ba8e098df202093b21973df0255fad78f747
Akram <khalilovakram07@gmail.com>
2024-06-07 17:54:18 +0600

    added new modules

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,46 +1,54 @@
 import { Link } from 'react-router-dom';
 import styles from './header.module.css'
+import { AccountBoxOutlined, Search, ShoppingBasket } from '@material-ui/icons';
 
 const Header = () => {
     return (
         <>
             <header className={styles.header}>
                 <div className={styles.headerTop}>
-                    <select>
-                        <option value="">Санкт-петербург</option>
-                        <option value="">Санкт-петербург</option>
-                        <option value="">Санкт-петербург</option>
-                    </select>
-                    <p> +8 495 259 25 00</p>
+                    <div className="container">
+                        <div className={styles.headerTopContent}>
+                            <select className={styles.headerTop__select}>
+                                <option value="">Санкт-петербург</option>
+                                <option value="">Санкт-петербург</option>
+                                <option value="">Санкт-петербург</option>
+                            </select>
+                            <p> +8 495 259 25 00</p>
+                        </div>
+                    </div>
+
                 </div>
-                <nav className={styles.nav}>
-                    <ul className={styles.links}>
+                <div className="container">
+                    <nav className={styles.nav}>
+                        <ul className={styles.links}>
 
-                        <div className={styles.headerLeft}>
-                            <Link to='/'>
-                                <div className={styles.logo}>
+                            <div className={styles.headerLeft}>
+                                <Link to='/'>
+                                    <div className={styles.logo}>
+                                        <img src="./images/logo.png" alt="" />
+                                    </div>
+                                </Link>
+                                <li>
+                                    <Link className={styles.link} to='/'>Каталог</Link>
+                                </li>
+                                <li>
+                                    <Link className={styles.link} to='/'>Контакты</Link>
+                                </li>
+                                <li>
+                                    <Link className={styles.link} to='/'>Блог</Link>
+                                </li>
+                            </div>
 
-                                </div>
-                            </Link>
-                            <li>
-                                <Link className={styles.link} to='/'>Каталог</Link>
-                            </li>
-                            <li>
-                                <Link className={styles.link} to='/'>Контакты</Link>
-                            </li>
-                            <li>
-                                <Link className={styles.link} to='/'>Блог</Link>
-                            </li>
-                        </div>
+                            <div className={styles.headerRight}>
+                                <Search />
+                                <AccountBoxOutlined />
+                                <ShoppingBasket />
+                            </div>
 
-                        <div className={styles.headerRight}>
-                            <p>Search</p>
-                            <p>Account</p>
-                            <p>Basket</p>
-                        </div>
-
-                    </ul>
-                </nav>
+                        </ul>
+                    </nav>
+                </div>
             </header>
         </>
     );
